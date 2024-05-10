f9059088c294e67d7336670a5f5c6fc71488e40a
Evgeny Gerasimov <e.gerasimov@mail.com>
2024-05-10 21:10:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -228,4 +228,4 @@ async def startup_event():
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("main:app", host="83.222.9.2", port=80, reload=True)
